392a51036df31ecdfbc032449f8f1113198a46ce
Yiping Kang <yipingkang93@gmail.com>
2024-12-09 17:06:45 -0500

    update UI

--- helpers.py
+++ helpers.py
@@ -35,10 +35,10 @@ def text_to_speech(input_text):
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
         data = f.read()
-    b64 = base64.b64encode(data).decode("utf-8")
-    md = f"""
-    <audio autoplay>
-    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-    </audio>
-    """
-    st.markdown(md, unsafe_allow_html=True)
+        b64 = base64.b64encode(data).decode("utf-8")
+        md = f"""
+        <audio autoplay>
+        <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+        </audio>
+        """
+        st.markdown(md, unsafe_allow_html=True)
